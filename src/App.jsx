93de9399bb839de77215f40276bef4828fe916ed
Caspessoa <cassianopessoa2003@hotmail.com>
2026-03-29 17:07:46 -0300

    feat: adição do header v1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header.jsx";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            {/* restante do conteudo da landing page */}
+            
+        </div>
+    )
+}
+
+export default App;
