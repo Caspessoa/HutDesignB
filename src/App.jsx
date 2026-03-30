25bc70ec391f578fde4853db260813aed9f19d89
Caspessoa <cassianopessoa2003@hotmail.com>
2026-03-30 18:10:52 -0300

    feat: adição do header v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header.jsx";
+import { Historia } from "./sections/Landing2/Historia.jsx";
 
 function App() {
     return (
         <div>
             <Header />
+            <Historia />
             {/* restante do conteudo da landing page */}
             
         </div>
