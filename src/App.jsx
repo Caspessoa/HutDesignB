fc9caaeff38b8e72199b9279225c600cb4fad4c7
Pedro Henrique Mognon De Bortoli <pedro.mbortoli@gmail.com>
2026-04-04 11:12:57 -0300

    Organizando repositório e ignorando node_modules

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import SolucoesDigitais from './sections/Landing2/Solucoes_cards';
+
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      
+      <main>
+
+        <SolucoesDigitais/>
+    
+      </main>
+
+     
+    </div>
+  );
+}
+
+export default App;
