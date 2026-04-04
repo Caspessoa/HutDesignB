be89be79e96499970bd890b608cfc3ff49064f56
Pedro Henrique Mognon De Bortoli <pedro.mbortoli@gmail.com>
2026-04-04 14:51:19 -0300

    style: move as estilizações da seção dos cards para arquivo CSS próprio

--- src/sections/Landing2/Solucoes_cards.jsx
+++ src/sections/Landing2/Solucoes_cards.jsx
@@ -27,7 +27,6 @@ export default function SolucoesDigitais() {
 
         </div>
 
-       
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 px-4 justify-items-center mt-2">
 
