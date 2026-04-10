d5df21ea14dab1a86a62fef11f490d27ff254489
Caspessoa <cassianopessoa2003@hotmail.com>
2026-04-10 12:19:34 -0300

    feat: adição de navegação useState

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import logoHut from '../assets/hut.svg';
 
-export function Header(){
+export default function Header({ setSecaoAtiva }){
     return (
         <header className="border-b border-gray-200 w-full bg-white sticky top-0 z-50">
             <div className="max-w-7xl mx-auto px-17 py-2 flex flex-wrap justify-between items-center gap-4">
@@ -14,14 +14,15 @@ export function Header(){
                 
                 <nav className="flex flex-wrap items-center gap-4 md:gap-10">
                     <ul className="flex flex-wrap gap-4 md:gap-6">
-                        <li><a href="#home" className="text-gray-600 font-medium">Home</a></li>
-                        <li><a href="#projetos" className="text-gray-600 font-medium">Projetos</a></li>
-                        <li><a href="#servicos" className="text-gray-600 font-medium">Serviços</a></li>
-                        <li><a href="#equipe" className="text-gray-600 font-medium">Nossa Equipe</a></li>
+                        <li><button onClick={() => setSecaoAtiva('home')} className="text-gray-600 font-medium hover:text-purple-800 cursor-pointer">Home</button></li>
+                        <li><button onClick={() => setSecaoAtiva('projetos')} className="text-gray-600 font-medium hover:text-purple-800 cursor-pointer">Projetos</button></li>
+                        <li><button onClick={() => setSecaoAtiva('servicos')} className="text-gray-600 font-medium hover:text-purple-800 cursor-pointer">Serviços</button></li>
+                        <li><button onClick={() => setSecaoAtiva('equipe')} className="text-gray-600 font-medium hover:text-purple-800 cursor-pointer">Nossa Equipe</button></li>
+                        
                     </ul>
-                    <a href="#contato" className="bg-purple-800 text-white font-medium px-6 py-2 rounded-full shadow-lg shadow-purple-800/40 hover:bg-purple-900 transition-colors">
+                    <button onClick={() => setSecaoAtiva('contato')} className="bg-purple-800 text-white font-medium px-6 py-2 rounded-full shadow-lg shadow-purple-800/40 hover:bg-purple-900 cursor-pointer">
                     Fale Conosco
-                    </a>
+                    </button>
                 </nav>  
 
             </div>
