85c088f197906b32612fff66447290cfde1ab0d5
Pedro Henrique Mognon De Bortoli <pedro.mbortoli@gmail.com>
2026-04-12 21:22:53 -0300

    feat: adiciona o grid da seção e os cards estilizados

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ import { QuemSomos } from "./sections/Landing2/quemSomos.jsx";
 import { Orcamento } from "./sections/Landing2/Orcamento.jsx";
 import Footer from "./components/Footer.jsx";
 import {Section3} from './sections/Landing2/Section3/Section3';
+import {Section2} from './sections/Projetos/Section2/Section2'
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="container-section2 min-h-screen flex flex-col bg-gray-50">
       
       {/* Conteúdo principal */}
       <div className="flex-grow">
@@ -19,6 +20,7 @@ function App() {
         <QuemSomos />
         <Section3 />
         <Orcamento />
+        <Section2 />
       </div>
 
       {/* Footer sempre no final */}
--- src/sections/Projetos/Section2/Section2.jsx
+++ src/sections/Projetos/Section2/Section2.jsx
@@ -3,16 +3,77 @@ import Vaca from '../../../assets/section2-projetos/pastejando-image.png';
 import Imovel from '../../../assets/section2-projetos/toni-neutzling-image.png';
 import Plantas from '../../../assets/section2-projetos/plante-saude-image.png';
 
-function Section2() {
+export function Section2() {
   return (
-    <section>
-      <h2>Teste de importação</h2>
+    <section className="w-full max-w-[1710px] mx-auto px-4 py-10">
 
-      <div>
+      <div className=" container-cards grid grid-cols-1 md:grid-cols-12 gap-[32px]" >
+       
+       {/* Card 1: Toni Neutzling*/}
+       <div className='md:col-span-8'>
+          <div 
+              style={{
+                // Removemos o width fixo daqui para usar o Tailwind
+                height: '498px', 
+                backgroundImage: `linear-gradient(180deg, rgba(14, 14, 18, 0) 0%, rgba(14, 14, 18, 0.4) 40%, rgba(14, 14, 18, 0.9) 90%)`
+              }}
+              className="w-full max-w-[1127px] rounded-3xl relative overflow-hidden flex flex-col justify-end p-6 md:p-10 mx-auto"
+            >
+              {/* Conteúdo */}
+          </div>
+        </div>
 
-      <></>
-
+        {/* Card 2: Plante Saúde */}
+        <div className="md:col-span-4">
+          <div 
+              style={{
+                backgroundColor: '#F5F2F9',
+                backdropFilter: 'blur(12px)',
+                WebkitBackdropFilter: 'blur(12px)',
+                boxShadow: '0px 4px 6px -4px rgba(0,0,0,0.1), 0px 10px 15px -3px rgba(0,0,0,0.1)'
+              }}
+              className="w-full max-w-[548px] h-[400px] md:h-[500px] rounded-[24px] border border-[#B2B1BA1A] flex flex-col p-6 md:p-8 relative overflow-hidden mx-auto"
+            >
+              {/* imagem do card */}
+              <div className="relative z-10">
+                {/* textos */}
+              </div>
+            </div>
+        </div>
+      
+        {/* Card 3: Pastejando */}
+        <div className="md:col-span-5">
+            <div 
+              style={{
+                backgroundColor: '#FFFFFF',
+                boxShadow: '0px 4px 6px -4px rgba(0, 0, 0, 0.1), 0px 10px 15px -3px rgba(0, 0, 0, 0.1)'
+              }}
+              className="w-full max-w-[694px] h-[400px] md:h-[494px] rounded-[24px] border border-[#B2B1BA33] flex flex-col p-6 md:p-8 relative overflow-hidden mx-auto"
+            >
+              {/* Imagem do card*/}
+              <div className="relative z-10 h-full flex flex-col justify-between">
+                {/* textos */}
+              </div>
+          </div>
+        </div>  
+        
+        {/* Card 4: Hut Admin */}
+        <div className="md:col-span-7">
+          <div 
+            style={{
+              backgroundColor: '#FFFFFF',
+              boxShadow: '0px 4px 6px -4px rgba(0, 0, 0, 0.1), 0px 10px 15px -3px rgba(0, 0, 0, 0.1)'
+            }}
+            className="w-full max-w-[984px] h-[400px] md:h-[494px] rounded-[24px] border border-[#B2B1BA33] flex flex-col p-6 md:p-10 relative overflow-hidden mx-auto"
+          >
+            {/* imagem do card*/}
+            <div className="relative z-10 h-full flex flex-col justify-center">
+              {/* texto */}
+            </div>
+          </div>
+        </div>
       </div>
+        
       
       <img src={Computador} alt="Dashboard Hut Admin" />
     
@@ -24,5 +85,3 @@ function Section2() {
     </section>
   );
 }
-
-export default Section2;
